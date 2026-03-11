d592bd84f3edb1922084f3824ed9286143619329
Chook Yao Yu <chookyaoyu18@gmail.com>
2026-03-11 15:27:50 +0800

    Upload Sailor2 and SEA-LION model download script

--- live_translator_app/android/app/src/main/kotlin/com/example/live_translator_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.live_translator_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ model_download/SAILOR_download.py
@@ -0,0 +1,8 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="sail/Sailor2-8B-Chat")
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe(messages)
